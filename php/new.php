0d44abc289bfdcb20f8696933e95a938cf621c62
Niranjan kumar <gowithnk@gmail.com>
2023-04-25 15:01:06 +0530

    html email styling

--- php/new.php
+++ php/new.php
@@ -34,17 +34,14 @@ $person_meet=$fetch['name'];
 if($fetch['phone']==''){$empPhone=$fetch['office_phone'];}else{$empPhone=$fetch['phone'];};
 
 $img = $_POST['image'];
-    $folderPath = "upload/";
-  
-    $image_parts = explode(";base64,", $img);
-    $image_type_aux = explode("image/", $image_parts[0]);
-    $image_type = $image_type_aux[1];
-  
-    $image_base64 = base64_decode($image_parts[1]);
-    $fileName = uniqid() . '.jpeg';
-  
-    $file = $folderPath . $fileName;
-    file_put_contents($file, $image_base64);	
+$folderPath = "upload/";
+$image_parts = explode(";base64,", $img);
+$image_type_aux = explode("image/", $image_parts[0]);
+$image_type = $image_type_aux[1];
+$image_base64 = base64_decode($image_parts[1]);
+$fileName = uniqid() . '.jpeg';
+$file = $folderPath . $fileName;
+file_put_contents($file, $image_base64);	
 
 //SMS
 $smsMsg = 'New Visitor :  Name - ' .$name .', Phone Number - ' .$phone .', Entry Time - ' .$in_time; 
@@ -107,22 +104,32 @@ try {
     );
     //Server settings
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'sh005.bigrock.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'sh005.bigrock.com';                    //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'gpms@synokem.com';                     //SMTP username
-    $mail->Password   = 'Syn@12345^^';                              //SMTP password
+    $mail->Password   = 'Syn@12345^^';                          //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
     
     $mail->setFrom('gpms@synokem.com', 'Synokem VMS');
-    $mail->addAddress($empEmail, 'Niranjan');     //Add a recipient
+    $mail->addAddress($empEmail, $person_meet);     //Add a recipient
     $mail->addAttachment($file);         //Add attachments
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'New Visitor';
-    $mail->Body    = '<b>Name: </b>'. $name . '<br>' . '<b>Gender: </b>' . $gender . '<br>' . '<b>Phone: </b>' . $phone . '<br>' . '<b>Company: </b>' . $company . '<br>' . '<b>Purpose: </b>' . $purpose . '<br>' . '<b>Person to Meet: </b>' . $person_meet . '<br>' . '<b>Date: </b>' . $date . '<br>' . '<b>Time: </b>' . $in_time . '<br>';
+    // $mail->Body    = '<b>Name: </b>'. $name . '<br>' . '<b>Gender: </b>' . $gender . '<br>' . '<b>Phone: </b>' . $phone . '<br>' . '<b>Company: </b>' . $company . '<br>' . '<b>Purpose: </b>' . $purpose . '<br>' . '<b>Person to Meet: </b>' . $person_meet . '<br>' . '<b>Date: </b>' . $date . '<br>' . '<b>Time: </b>' . $in_time . '<br>';
+    $mail->Body    = '<table bgcolor="#e4f3ff" cellpadding="5" cellspacing="5" style="width="100%;padding:30px;">
+      <tr><td><b>Name : </b></td><td>'. $name . '</td></tr>
+      <tr><td><b>Gender : </b></td><td>'. $gender . '</td></tr>
+      <tr><td><b>Phone : </b></td><td>'. $phone . '</td></tr>
+      <tr><td><b>Company : </b></td><td>'. $company . '</td></tr>
+      <tr><td><b>Purpose : </b></td><td>'. $purpose . '</td></tr>
+      <tr><td><b>Person to Meet : </b></td><td>'. $person_meet . '</td></tr>
+      <tr><td><b>Date : </b></td><td>'. $date . '</td></tr>
+      <tr><td><b>Time : </b></td><td>'. $in_time . '</td></tr>
+    </table>';
+
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
@@ -136,14 +143,15 @@ $sql="insert into inquery(Name,Gender,Phone,Department,Person_Meet,In_Time,Date,
 $query=mysqli_query($db,$sql); 
 if($query) 
 		{     
-			echo"<script>alert('Data Inserted'); 
-		     	location.href='slip.php';
-					</script>";	
+			echo"<script>alert('Data Inserted');</script>";
+      die();	
+      echo"<script>location.href='slip.php';</script>";
 		}
 		else
 		{
 			echo "<br>";
 			echo"<script>alert ('error');</script>";
+      die();
 		}
 		
 }
